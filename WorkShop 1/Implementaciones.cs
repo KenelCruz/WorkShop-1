f430b5bbc312e8027fc98bc2e1790e223851b296
Kenel Cruz <kenelcruz@gmail.com>
2021-06-20 21:27:02 -0400

    Agregar archivos de proyecto.

---
+++ WorkShop 1/Implementaciones.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WorkShop_1
+{
+    //Clase Padre o Base
+    class Implementaciones
+    {
+        public void MenuPrincipal()
+        {
+
+            Console.Clear();
+
+
+            Console.WriteLine("--------------------Bienvenido a este programa-------------------");
+            Console.WriteLine("                                       ");
+            Console.WriteLine("Ingrese un numero");
+            Console.WriteLine("1- Autor    |   2- For");
+            Console.WriteLine("3- Do While |   4-Foreach");
+            Console.WriteLine("                                 ");
+
+            //var
+           var respuesta = Console.ReadLine();
+
+            switch (respuesta)
+            {
+                case "1":
+                    Primero();
+                    break;
+                case "2":
+                    Segundo();
+                    break;
+                case "3":
+                    Tercero();
+                    break;
+                case "4":
+                    Cuarto();
+                    break;
+
+                default:
+                    // code block
+                    break;
+            }
+        }
+
+
+        public void Primero()
+        {
+            Console.Clear();
+            //Const
+            const string Nombre = "Kenel_Cruz";
+            Console.WriteLine("--------------------Autor-------------------");
+            Console.WriteLine("                                       ");
+            Console.WriteLine(Nombre);
+            Console.WriteLine("                                       ");
+            Console.WriteLine("Presione 0 para volver atras");
+
+            //Var
+            var res = Console.ReadLine();
+
+            if(res == "0")
+            {
+                MenuPrincipal();
+            }
+        }
+
+        public void Segundo()
+        {
+            Console.Clear();
+            Console.WriteLine("--------------------For-------------------");
+            Console.WriteLine("                                       ");
+
+            for (int i = 0; i < 50; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+
+
+            Console.WriteLine("Presione 0 para volver atras");
+
+            //Var
+            var res = Console.ReadLine();
+
+            if (res == "0")
+            {
+                MenuPrincipal();
+            }
+
+        }
+
+        public void Tercero()
+        {
+            Console.Clear();
+            Console.WriteLine("--------------------Do While-------------------");
+            Console.WriteLine("                                       ");
+            int i = 0;
+            do
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+            while (i < 5);
+
+            Console.WriteLine("Presione 0 para volver atras");
+
+            //Var
+            var res = Console.ReadLine();
+
+            if (res == "0")
+            {
+                MenuPrincipal();
+            }
+        }
+
+        public void Cuarto()
+        {
+            Console.Clear();
+            Console.WriteLine("--------------------Do While-------------------");
+            Console.WriteLine("                                       ");
+
+            string[] Carros = { "Volvo", "BMW", "Ford", "Mazda" };
+            foreach (string i in Carros)
+            {
+                Console.WriteLine(i);
+            }
+
+
+
+            Console.WriteLine("Presione 0 para volver atras");
+
+            //Var
+            var res = Console.ReadLine();
+
+            if (res == "0")
+            {
+                MenuPrincipal();
+            }
+
+        }
+
+
+
+
+    }
+}
---
+++ WorkShop 1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace WorkShop_1
+{
+    //Clase Hija
+    class Program : Implementaciones
+    {
+        public static void Main()
+        {
+            new Program().MenuPrincipal();
+        }
+       
+    }
+
+    //Clase 
+    public class NuevaClass
+    {
+        //Nuleable
+        int? Dias_de_trabajo = default;
+
+        public void Menu()
+        {
+            Console.WriteLine("Bienvenido al Menu");
+        }
+
+    }
+
+    //Interface
+    interface INuevaclass
+    {
+      void Menu();
+    }
+
+    //Type parameters
+    public class Parametros<T>{
+        //Objeto o intencia de la clase
+        NuevaClass Nuevaclase = new NuevaClass();
+        
+
+        public T Propiedad  { get; set; }
+    }
+
+
+    //Enums
+    public enum Dias
+    {
+        Lunes = 0,
+        Martes = 1,
+        Miercoles = 2,
+        Jueves = 3,
+        Viernes = 4,
+        Sabado = 5,
+        Domingo = 6
+    }
+
+   
+}
